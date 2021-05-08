4d15cf139efdaf1ec81c4e4e73aca22ec6080303
Geordan9 <geo3125@gmail.com>
2021-05-08 11:01:08 -0600

    Fixed min name length option and added endianness option.

--- GeoArcSysPACker/Program.cs
+++ GeoArcSysPACker/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using ArcSysAPI.Common.Enums;
 using ArcSysAPI.Models;
 using GeoArcSysPACker.Models;
 using GeoArcSysPACker.Utils.Extensions;
@@ -17,7 +18,9 @@ namespace GeoArcSysPACker
             MinNameLength = 0x1,
             Recursive = 0x2,
             NameID = 0x4,
-            NameIDExt = 0x8
+            NameIDExt = 0x8,
+            Endianness = 0x8000000,
+            Continue = 0x40000000
         }
 
         public static ConsoleOption[] ConsoleOptions =
@@ -29,6 +32,7 @@ namespace GeoArcSysPACker
                 LongOp = "--minnamelength",
                 Description =
                     "Specifies the minimum file name length. {Auto} will \n\t\t\tautomatically calculate it.",
+                HasArg = true,
                 Flag = Options.MinNameLength
             },
             new ConsoleOption
@@ -57,14 +61,32 @@ namespace GeoArcSysPACker
                 Description =
                     "Applies a unique ID based the file's name. (64 character limit)",
                 Flag = Options.NameIDExt
+            },
+            new ConsoleOption
+            {
+                Name = "Endianness",
+                ShortOp = "-en",
+                LongOp = "--endianness",
+                Description = "Specify the output file's endianness.",
+                HasArg = true,
+                Flag = Options.Endianness
+            },
+            new ConsoleOption
+            {
+                Name = "Continue",
+                ShortOp = "-c",
+                LongOp = "--continue",
+                Description = "Don't pause the application when finished.",
+                Flag = Options.Continue
             }
         };
 
         public static string assemblyPath = string.Empty;
         private static bool AlwaysOverwrite;
-        private static int MinNameLength = 0;
+        private static int MinNameLength;
 
         public static Options options;
+        public static ByteOrder endianness = ByteOrder.LittleEndian;
 
         [STAThread]
         private static void Main(string[] args)
@@ -74,135 +96,136 @@ namespace GeoArcSysPACker
             assemblyPath = Path.GetFullPath(Uri.UnescapeDataString(uri.Path));
 
             Console.ForegroundColor = ConsoleColor.DarkGray;
-            Console.WriteLine("\nGeo BlazBlue Packer\nprogrammed by: Geo\n\n");
+            Console.WriteLine("\nGeo ArcSys Packer\nprogrammed by: Geo\n\n");
             Console.ForegroundColor = ConsoleColor.White;
 
             ProcessOptions(args);
 
             try
             {
-                if (args.Length > 0)
-                {
-                    var procedureType = Procedure.Pack;
-
-                    if (args.Length > 1)
-                    {
-                        args[1] = args[1].ToLower().FirstCharToUpper();
-                        byte b;
-                        if (byte.TryParse(args[1], out b))
-                        {
-                            if (b <= 1)
-                                procedureType = (Procedure) b;
-                        }
-                        else if (Enum.IsDefined(typeof(Procedure), args[1]))
-                        {
-                            procedureType = (Procedure) Enum.Parse(typeof(Procedure), args[1]);
-                        }
-                    }
-
-                    var path = Path.GetFullPath(args[0]);
-
-                    var attr = new FileAttributes();
-                    try
-                    {
-                        attr = File.GetAttributes(path);
-                    }
-                    catch
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine($"Can't retrieve \"{path}\" file system attributes.");
-                        Console.ForegroundColor = ConsoleColor.White;
-                        return;
-                    }
-
-
-                    if (!attr.HasFlag(FileAttributes.Directory)) procedureType = Procedure.Unpack;
-
-                    if (procedureType == Procedure.Pack)
-                    {
-                        if (!Directory.Exists(path))
-                        {
-                            Console.WriteLine($"The \"{path}\" directory does not exist.");
-                            return;
-                        }
-
-                        var savePath = path + ".pac";
-                        var createExtNameID = options.HasFlag(Options.NameIDExt);
-
-                        var createNameID = options.HasFlag(Options.NameID) ||
-                                           createExtNameID;
-                        var pacParams =
-                            createExtNameID ? PACFileInfo.PACParameters.GenerateExtendedNameID :
-                            createNameID ? PACFileInfo.PACParameters.GenerateNameID : 0;
-
-                        File.WriteAllBytes(savePath, new PACFileInfo(path, pacParams, MinNameLength).GetBytes());
-                    }
-                    else
-                    {
-                        var paths = new List<string>();
-
-                        var baseDirectory = string.Empty;
-
-                        var saveFolder = string.Empty;
-
-                        var isDirectory = attr.HasFlag(FileAttributes.Directory);
-
-                        var isRecursive = options.HasFlag(Options.Recursive);
-
-                        if (isRecursive)
-                        {
-                            baseDirectory = path;
-                            saveFolder = baseDirectory + "_unpack";
-                        }
-
-                        if (isRecursive && isDirectory)
-                            paths.AddRange(DirSearch(path));
-                        else if (isDirectory)
-                            paths.AddRange(Directory.GetFiles(path));
-                        else
-                            paths.Add(path);
-
-                        foreach (var filePath in paths)
-                        {
-                            if (!File.Exists(filePath))
-                            {
-                                Console.WriteLine($"The \"{filePath}\" file does not exist.");
-                                continue;
-                            }
-
-                            if (!isRecursive) saveFolder = baseDirectory = Directory.GetParent(filePath).FullName;
-
-                            var mainPACFile = new PACFileInfo(filePath, MinNameLength);
-
-                            mainPACFile.Active = true;
-
-                            if (!mainPACFile.IsValidPAC)
-                            {
-                                Console.WriteLine($"{mainPACFile.Name} is not a valid PAC file.");
-                                continue;
-                            }
-
-                            ProcessFile(mainPACFile, baseDirectory, saveFolder);
-
-                            var vfiles = RecursivePACExplore(mainPACFile);
-
-                            foreach (var vfile in vfiles) ProcessFile(vfile, baseDirectory, saveFolder);
-
-                            mainPACFile.Active = false;
-                        }
-                    }
-
-                    Console.WriteLine();
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Done!");
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine();
-                }
-                else
+                if (args.Length == 0 ||
+                    args.Length > 0 && (args[0] == "-h" || args[0] == "--help"))
                 {
                     ShowUsage();
                     Pause();
+                    return;
                 }
+
+                var procedureType = Procedure.Pack;
+
+                if (args.Length > 1)
+                {
+                    args[1] = args[1].ToLower().FirstCharToUpper();
+                    byte b;
+                    if (byte.TryParse(args[1], out b))
+                    {
+                        if (b <= 1)
+                            procedureType = (Procedure) b;
+                    }
+                    else if (Enum.IsDefined(typeof(Procedure), args[1]))
+                    {
+                        procedureType = (Procedure) Enum.Parse(typeof(Procedure), args[1]);
+                    }
+                }
+
+                var path = Path.GetFullPath(args[0]);
+
+                var attr = new FileAttributes();
+                try
+                {
+                    attr = File.GetAttributes(path);
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Can't retrieve \"{path}\" file system attributes.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    return;
+                }
+
+
+                if (!attr.HasFlag(FileAttributes.Directory)) procedureType = Procedure.Unpack;
+
+                if (procedureType == Procedure.Pack)
+                {
+                    if (!Directory.Exists(path))
+                    {
+                        Console.WriteLine($"The \"{path}\" directory does not exist.");
+                        return;
+                    }
+
+                    var savePath = path + ".pac";
+                    var createExtNameID = options.HasFlag(Options.NameIDExt);
+
+                    var createNameID = options.HasFlag(Options.NameID) ||
+                                       createExtNameID;
+                    var pacParams =
+                        createExtNameID ? PACFileInfo.PACParameters.GenerateExtendedNameID :
+                        createNameID ? PACFileInfo.PACParameters.GenerateNameID : 0;
+
+                    File.WriteAllBytes(savePath,
+                        new PACFileInfo(path, pacParams, MinNameLength, endianness).GetBytes());
+                }
+                else
+                {
+                    var paths = new List<string>();
+
+                    var baseDirectory = string.Empty;
+
+                    var saveFolder = string.Empty;
+
+                    var isDirectory = attr.HasFlag(FileAttributes.Directory);
+
+                    var isRecursive = options.HasFlag(Options.Recursive);
+
+                    if (isRecursive)
+                    {
+                        baseDirectory = path;
+                        saveFolder = baseDirectory + "_unpack";
+                    }
+
+                    if (isRecursive && isDirectory)
+                        paths.AddRange(DirSearch(path));
+                    else if (isDirectory)
+                        paths.AddRange(Directory.GetFiles(path));
+                    else
+                        paths.Add(path);
+
+                    foreach (var filePath in paths)
+                    {
+                        if (!File.Exists(filePath))
+                        {
+                            Console.WriteLine($"The \"{filePath}\" file does not exist.");
+                            continue;
+                        }
+
+                        if (!isRecursive) saveFolder = baseDirectory = Directory.GetParent(filePath).FullName;
+
+                        var mainPACFile = new PACFileInfo(filePath, MinNameLength);
+
+                        mainPACFile.Active = true;
+
+                        if (!mainPACFile.IsValidPAC)
+                        {
+                            Console.WriteLine($"{mainPACFile.Name} is not a valid PAC file.");
+                            continue;
+                        }
+
+                        ProcessFile(mainPACFile, baseDirectory, saveFolder);
+
+                        var vfiles = RecursivePACExplore(mainPACFile);
+
+                        foreach (var vfile in vfiles) ProcessFile(vfile, baseDirectory, saveFolder);
+
+                        mainPACFile.Active = false;
+                    }
+                }
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Done!");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine();
             }
             catch (Exception ex)
             {
@@ -257,19 +280,31 @@ namespace GeoArcSysPACker
 
                 if (co.HasArg)
                 {
-                    var subArgs = (string[])co.SpecialObject;
-                    if ((Options)co.Flag == Options.MinNameLength &&
+                    var subArgs = (string[]) co.SpecialObject;
+                    if ((Options) co.Flag == Options.MinNameLength &&
                         options.HasFlag(Options.MinNameLength))
                     {
                         int length;
                         if (subArgs.Length > 0 && subArgs[0].ToLower() == "auto")
-                        {
                             length = 0;
-                        }
                         else if (subArgs.Length == 0 || !int.TryParse(subArgs[0], out length))
                             length = 24;
-                        
+
                         MinNameLength = length;
+                    }
+
+                    if ((Options) co.Flag == Options.Endianness &&
+                        options.HasFlag(Options.Endianness))
+                    {
+                        if (subArgs.Length > 0)
+                        {
+                            ByteOrder endian;
+                            if (Enum.TryParse(subArgs[0], true, out endian)) endianness = endian;
+                        }
+                        else
+                        {
+                            endianness = ByteOrder.LittleEndian;
+                        }
                     }
                 }
             }
@@ -398,8 +433,11 @@ namespace GeoArcSysPACker
                     $"{co.ShortOp.PadRight(shortOpMaxLength)}\t{co.LongOp.PadRight(longOpMaxLength)}\t{co.Description}");
         }
 
-        private static void Pause()
+        private static void Pause(bool force = false)
         {
+            if (options.HasFlag(Options.Continue) && !force)
+                return;
+
             Console.WriteLine("\rPress Any Key to exit...");
             Console.ReadKey();
         }
